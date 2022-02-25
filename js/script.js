88b5679ddf5c27d0e9fc283cc7ef467d17b311a7
MateuszPuszcz <mateuszpuszcz.official@gmail.com>
2022-02-25 11:25:43 +0100

    tło elementów

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('WARSZTAT - Ustaw tło elementów HTML')
+
+const setBackground = () => {
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+
+    p1.style.backgroundColor = 'red';
+    p2.style.backgroundColor = 'yellow';
+    
+    // console.log(p2);
+}
+
+let btnSetBackground = document.getElementById('set-background');
+
+btnSetBackground.addEventListener('click', setBackground);
