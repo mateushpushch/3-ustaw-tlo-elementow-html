661b00e473affbfc4aa8bdf26a16f2925dad264d
MateuszPuszcz <mateuszpuszcz.official@gmail.com>
2022-03-17 23:31:12 +0100

    dom

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-console.log('WARSZTAT - Ustaw tło elementów HTML')
+console.log(`WARSZTAT - Ustaw tło elementów HTML`);
 
 const setBackground = () => {
     let p1 = document.querySelector('.first');
     let p2 = document.querySelector('.last');
 
-    p1.style.backgroundColor = 'red';
-    p2.style.backgroundColor = 'yellow';
-    
-    // console.log(p2);
+    p1.classList.toggle('bg-red');
+    p2.classList.toggle('bg-yellow');
+
+    //p1.style.backgroundColor = 'red';
+    //p2.style.backgroundColor = 'yellow';
 }
 
 let btnSetBackground = document.getElementById('set-background');
